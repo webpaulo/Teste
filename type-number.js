68f8d587716f7bc13503fa710e402fc2a3c0c2a9
unknown <webpaulo@gmail.com>
2022-03-02 10:35:04 -0300

    exemplo

---
+++ type-number.js
@@ -0,0 +1,16 @@
+// tipo Number
+
+const meuNumero = 3;
+
+
+const primeiroNumero = 2.6;
+const segundoNumero = 3;
+
+const OperacaoMatematica =primeiroNumero / segundoNumero;
+
+console.log(OperacaoMatematica)
+
+//NaN
+const  alura = "Alura";
+console.log(alura*3)
+
---
+++ type-string.js
@@ -0,0 +1,19 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "Senha Super Segura456";
+const stringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+
+const meuNome = "Leonardo";
+
+console.log(citacao)
+
+console.log(citacao+"\u2705"+meuNome)
+//concatenação
+
+
+
+
+//template string
+
